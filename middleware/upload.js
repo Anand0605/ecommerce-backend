4666074b5f73eb3a39f04334a3e8c76562b8ab56
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-06-29 03:29:49 +0530

    update

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,6 @@
 // middleware/upload.js
 const multer = require('multer');
-// const path = require('path');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
